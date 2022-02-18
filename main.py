fd0068d9a49232ab281a2acea69c4103b7353465
bulra <bulray30@gmail.com>
2022-02-18 22:34:24 +0300

    1 first

--- main.py
+++ main.py
@@ -1,39 +1,39 @@
 from flask import Flask, request, render_template, helpers, debughelpers
-import utils
-
-# from utils import get_posts_all, get_posts_by_user, get_post_by_pk, search_for_posts
+from utils import get_post_by_pk, get_posts_by_user, search_for_posts, get_comments_by_user, get_posts_all
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def page_index():
-    posts = utils.get_posts_all()
+    posts = get_posts_all()
     return render_template('index.html', posts=posts)
 
 
 @app.route('/posts/<int:post_pk>')
 def pag_single_post(post_pk):
-    post = utils.get_post_by_pk(post_pk)
-    comments = utils.get_comments_by_user(post_pk)
+    post = get_post_by_pk(post_pk)
+    comments = get_comments_by_user(post_pk)
     comments_len = len(comments)
-    return render_template("post.html", post=post, comments = comments, comments_len=comments_len)
+    return render_template("post.html", post=post, comments=comments, comments_len=comments_len)
+
 
 @app.route('/search')
 def page_search():
     query = request.args.get("s")
-    if not query or query == "":
+    if not query:
         return "вы ничего не искали"
-    posts = utils.search_for_posts(query)
+    posts = search_for_posts(query)
     posts_count = len(posts)
     return render_template("search.html", posts=posts, posts_count=posts_count, query=query)
 
+
 @app.route("/user/<user_name>")
 def page_user(user_name):
-    posts = utils.get_posts_by_user(user_name)
+    posts = get_posts_by_user(user_name)
     posts_count = len(posts)
 
     return render_template("user-feed.html", posts=posts, posts_count=posts_count, user_name=user_name)
 
-if __name__ == '__name__':
-  app.run(debug=True)
+app.run()
+
--- utils.py
+++ utils.py
@@ -1,11 +1,18 @@
-import json
+import json, os
 import pprint
 
 
 def get_posts_all():
     with open("data/data.json", "r", encoding="utf-8") as file:
         data = json.load(file)
-    return data
+        if os.path.exists('data/data.json'):
+          return data
+        else:
+            return None
+"""
+отображается вс посты
+"""
+
 
 
 def get_posts_by_user(user_name):
@@ -15,7 +22,9 @@ def get_posts_by_user(user_name):
         if post["poster_name"] == user_name:
             posts_by_user.append(post)
     return posts_by_user
-
+"""
+отображает посты выбранного пользователя
+"""
 
 def search_for_posts(query):
     posts = get_posts_all()
@@ -26,18 +35,29 @@ def search_for_posts(query):
         if query_lower in post["content"].lower():
             matching_posts.append(post)
     return matching_posts
-
+"""
+отображает список словарей
+"""
 
 def get_post_by_pk(pk):
     posts = get_posts_all()
     for post in posts:
         if post['pk'] == pk:
             return post
+        return None
+"""
+отображает пост по его ID
+"""
 
 def get_comments_all():
     with open("data/comments.json", "r", encoding="utf-8") as file:
         data = json.load(file)
-    return data
+        if os.path.exists('data/comments.json'):
+          return data
+        else:
+            return None
+"""
+возвращает комменатрии"""
 
 def get_comments_by_user(pk):
     comments = get_comments_all()
@@ -48,7 +68,6 @@ def get_comments_by_user(pk):
             comments_for_posts.append(comment)
 
     return comments_for_posts
-
-
-
-
+"""
+возвращает комментарии определенного пользователя
+"""
